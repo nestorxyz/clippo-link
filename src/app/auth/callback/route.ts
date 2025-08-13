16370dd64a7cd838f9c1f4ee13ccd08d3e30a963
Nestor Mamani <nmamanipantoja@gmail.com>
2025-08-13 17:33:42 -0500

    feat: Implement Supabase session management with middleware and server integration

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from 'next/server';
+// The client you created from the Server-Side Auth instructions
+import { createClient } from '@/integrations/retired-provider/server';
+
+export async function GET(request: Request) {
+  console.log('Auth Callback Route Hit:', request.url);
+  const { searchParams, origin } = new URL(request.url);
+  const access_token = searchParams.get('access_token');
+  const provider_token = searchParams.get('provider_token');
+  // if "next" is in param, use it as the redirect URL
+  let next = searchParams.get('next') ?? '/';
+  if (!next.startsWith('/')) {
+    // if "next" is not a relative URL, use the default
+    next = '/';
+  }
+
+  console.log('Access Token:', access_token);
+  console.log('Provider Token:', provider_token);
+
+  if (access_token && provider_token) {
+    const retired-provider = await createClient();
+    const { data, error } = await retired-provider.auth.signInWithIdToken({
+      provider: 'google',
+      access_token,
+      token: provider_token,
+    });
+    console.log('retired-provider Auth Data:', data);
+    console.log('retired-provider Auth Error:', error);
+
+    if (!error) {
+      const forwardedHost = request.headers.get('x-forwarded-host'); // original origin before load balancer
+      const isLocalEnv = process.env.NODE_ENV === 'development';
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`);
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      } else {
+        return NextResponse.redirect(`${origin}${next}`);
+      }
+    }
+  }
+
+  // return the user to an error page with instructions
+  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+}
