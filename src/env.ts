200d5ec554cb6bbc75c6f16260a8ba2b8cbde4d4
Nestor Mamani <nmamanipantoja@gmail.com>
2026-01-23 21:13:38 -0500

    a

--- src/env.ts
+++ src/env.ts
@@ -7,8 +7,7 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_BACKEND_URL: z.string().url(),
-    /* NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-    NEXT_PUBLIC_CLERK_SIGN_IN_URL: z.string(), */
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
     NEXT_PUBLIC_CONVEX_URL: z.string().url(),
   },
   shared: {
@@ -17,6 +16,8 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
   },
