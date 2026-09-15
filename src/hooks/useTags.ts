b13bbf075b7992e90a22fc3c42052e77ebe9d65f
Nestor Mamani <nmamanipantoja@gmail.com>
2026-09-15 12:32:59 -0500

    refactor: remove legacy dashboard island

--- src/hooks/useTags.ts
+++
@@ -1,7 +0,0 @@
-import { useQuery } from 'convex/react';
-import { api } from '../../convex/_generated/api';
-
-export const useTags = () => {
-  const data = useQuery(api.tags.get);
-  return { data, isLoading: data === undefined };
-};
